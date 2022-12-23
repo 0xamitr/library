fd91659b5487919e3a79deeb7fe72d302958cd2d
amit-rathore0 <amitrathore.amit.ar.70@gmail.com>
2022-12-23 23:29:01 +0530

    ff

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let defbutton = document.getElementById("addbook");
 let form = document.getElementById("form");
 let demo = document.getElementById("demo");
 let i = 0;
+let numcard = 0;
 
 
 function Book(title, author, pages, read) {
@@ -23,12 +24,39 @@ function addBookToLibrary() {
     if (form.style.display != "none"){
         form.style.display = "none";
     }
-    demo.innerHTML += `<div><p>${myLibrary[i].title}</p>
+    demo.innerHTML += `<div id = "card${numcard}"><p>${myLibrary[i].title}</p>
                        <p>${myLibrary[i].author}</p>
                        <p>${myLibrary[i].pages}</p>
-                       <p>${myLibrary[i].read}</p></div>`
+                       <p>${myLibrary[i].read}</p>
+                       <button class = "common" data-number0 = "${i}">Remove</button></div>`
+                       numcard++;
+ 
+    let data = document.querySelectorAll('.common');
+    let tool = 0;
+    data.forEach(btn => {
+        btn.addEventListener('click', () => {
+            let parentDiv = btn.parentNode;
+            parentDiv.remove();
+            let length0 = myLibrary.length;
+            if (btn.dataset.number0 < length0 - 1){
+                let p = btn.dataset.number0;
+                for(p; p < length0; p++){
+                    let data1 = document.querySelector(`[data-number0 = "${p}"]`);
+                    if (p != btn.dataset.number0){
+                        console.log(data1);
+                        data1.dataset.number0--;
+                    }
+                }
+            }
+            myLibrary.splice(btn.dataset.number0, 1);
+            if (i > 0){
+                i--;
+            }
+        });
+    });
     i++;
 }
+
 function handleForm(event) { event.preventDefault(); } 
 form.addEventListener('submit', handleForm);
 button.addEventListener("click", addBookToLibrary)
